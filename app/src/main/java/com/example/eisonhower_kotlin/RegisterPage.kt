c5f5391145abac0291e13ba066d225f497003786
Benoit <benoit.hoffman@epitech.eu>
2020-01-28 09:38:32 +0100

    DEV - navigation OK

---
+++ app/src/main/java/com/example/eisonhower_kotlin/RegisterPage.kt
@@ -0,0 +1,15 @@
+package com.example.eisonhower_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register_page.*
+
+class RegisterPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_page)
+
+        test_box.text =  intent.getStringExtra("Name")
+    }
+}
