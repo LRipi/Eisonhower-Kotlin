ed50f26faf634766da76393c52d06c3dc97c88d3
Benoit <benoit.hoffman@epitech.eu>
2020-03-11 15:25:14 +0100

    DEV - added popup options for register page

--- app/src/main/java/com/example/eisonhower_kotlin/RegisterPage.kt
+++ app/src/main/java/com/example/eisonhower_kotlin/RegisterPage.kt
@@ -2,12 +2,30 @@ package com.example.eisonhower_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_edit_task.*
 import kotlinx.android.synthetic.main.activity_register_page.*
 
 class RegisterPage : AppCompatActivity() {
 
+    fun spawnPopup(message: String)
+    {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage(message)
+        builder.show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
+
+        val registerButton = findViewById<Button>(R.id.registerButton)
+        val emailTextBox = findViewById<EditText>(R.id.emailTextbox)
+
+        registerButton.setOnClickListener {
+            spawnPopup(emailTextBox.text.toString())
+        }
     }
 }
